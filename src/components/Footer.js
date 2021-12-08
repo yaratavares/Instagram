b32b76e3b2e2ce0a80251763797fd2a480f78bed
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-12-08 14:41:56 -0300

    Preparando revisão de código

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer (){
-    const icones = ["home-sharp", "search-outline", "add-circle-outline", "heart-outline", "person-outline"];
-    
-    return (
-        <footer>
-            {icones.map((icon, i) => <ion-icon name={icon} key = {"J" +i}></ion-icon>)}
-        </footer>
-    );
-}
